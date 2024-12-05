0af52797a072186829697bbbb2108323332d059c
svtslvtt <tup60276@temple.edu>
2024-12-05 12:00:13 -0500

    Last activity

---
+++ app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.myfirstcomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFirstComposeAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Counter()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Counter() {
+    var counter by rememberSaveable { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = {}
+        ) { Text("Increase")}
+            Text(
+                counter.toString(),
+                fontSize = 64.sp
+        )
+        Button(
+            onClick = {}
+        ) { Text("Decrease")}}
+        }
+
+
+@Preview(showBackground = true)
+
